e2398aa3aff75b2fbdb0ddc9d9f7cf36b920b7ee
tiredpancake <119611305+tiredpancake@users.noreply.github.com>
2024-11-20 02:17:03 +0330

    FIX :redesign  landingPage

--- src/Components/LandingPage/Components/Home.jsx
+++ src/Components/LandingPage/Components/Home.jsx
@@ -1,11 +1,11 @@
  
 import BusImg from "../Assets/bus.png";  
-import Navbar from "./header";   
+
 import { FiArrowRight } from "react-icons/fi";  
 import "./landing.scss";
 import axios  from "axios";
 import React, { useEffect, useState } from "react";
-
+import LogoImg from "../Assets/logo.png" 
 
 const Home = () => {
   const [data, setData] = useState(null);   
@@ -31,25 +31,50 @@ const Home = () => {
   if (error) return <div>Error: {error.message}</div>;  // Show error message if any   
   return (
     <div class="LandingForm">  
-    <div class="big-wrapper">
-      <Navbar />
-      <div class="showcase-area">
+      <div class="land-back">
+      <header>
+        <nav class="container">
+        <div class="logo">
+            <img src={LogoImg||data.logo_image_url} alt="Logo"></img>
+            <h3>{data.logo_name}</h3>
+        </div>
+        <div class="links">
+          <ul>
+            <li><a href="#">{data.home}</a></li>
+            <li><a href="#">{data.about}</a></li>
+            <li><a href="/signup">Signup</a></li>
+            <li><a href="/login">Login</a></li>
+          </ul>
+        </div>
+        </nav>
+      </header>    
+      <div class="content-center"> 
+        <h1 class="big-title">Start Your Adventure
+          
+        </h1> 
+        <a href="#" class="btn2">Let's Go<FiArrowRight /></a> 
+        {/* <div class="aurora">
+            <div class="aurora__item"></div>
+            <div class="aurora__item"></div>
+            <div class="aurora__item"></div>
+            <div class="aurora__item"></div>
+          </div> */}
+      </div>
+      {/* <div class="showcase-area">
         <div class="container">
           <div class="left">
             <p class="big-title">  
-                <div class="big-title">{data.text}</div>  
-              <div> <h2 class="primary-heading">{data.text3}</h2>  </div>
-                <div class="big-title"> {data.text4} </div>   
+                <div class="big-title">Start Your Adventure Today</div>   
             </p>
             <div class="cta"></div>
               <a href="#" class="btn2">{data.paginate}<FiArrowRight /></a>  
           </div>
-          <div class="right">
+          {/* <div class="right">
             <img src={ BusImg||data.image_url } alt="Bus" class="person"></img>
-          </div>
-          </div>
+          </div> */}
+          {/* </div> */}
+      {/* </div> */} 
       </div>
-    </div>
     </div>
   );  
 };  
--- src/Components/LandingPage/Components/header.jsx
+++ src/Components/LandingPage/Components/header.jsx
@@ -1,51 +1,52 @@
-import React, { useEffect, useState } from "react";
-import LogoImg from "../Assets/logo.png" 
-import { Link } from "react-router-dom";  
-import { IoHome } from "react-icons/io5"; // Import home icon  
-import './landing.scss'; // Ensure you import the correct stylesheet  
-import axios from "axios";
+// import React, { useEffect, useState } from "react";
+// import LogoImg from "../Assets/logo.png" 
+// import { Link } from "react-router-dom";  
+// import { IoHome } from "react-icons/io5"; // Import home icon  
+// import './landing.scss'; // Ensure you import the correct stylesheet  
+// import axios from "axios";
 
-const Navbar = () => {  
-  const [data, setData] = useState(null);   
-  const [loading, setLoading] = useState(true);   
-  const [error, setError] = useState(null);   
+// const Navbar = () => {  
+//   const [data, setData] = useState(null);   
+//   const [loading, setLoading] = useState(true);   
+//   const [error, setError] = useState(null);   
 
-  useEffect(() => {  
-    const fetchData = async () => {  
-      try {  
-        const response = await axios.get("https://TripTide.pythonanywhere.com/home/"); // Replace with your actual endpoint  
-        setData(response.data);  
-        } catch (err) {  
-        setError(err);   
-      } finally {  
-        setLoading(false); 
-      }  
-    };  
+//   useEffect(() => {  
+//     const fetchData = async () => {  
+//       try {  
+//         const response = await axios.get("https://TripTide.pythonanywhere.com/home/"); // Replace with your actual endpoint  
+//         setData(response.data);  
+//         } catch (err) {  
+//         setError(err);   
+//       } finally {  
+//         setLoading(false); 
+//       }  
+//     };  
 
-    fetchData();   
-  }, []);   
+//     fetchData();   
+//   }, []);   
 
-  if (loading) return <div>Loading...</div>;  
-  if (error) return <div>Error: {error.message}</div>;  
-  return (  
-    <header>
-      <div class="container">
-        <div class="logo">
-            <img src={LogoImg||data.logo_image_url} alt="Logo"></img>
-            <h3>{data.logo_name}</h3>
-        </div>
-        <div class="links">
-          <ul>
-            <li><a href="#">{data.home}</a></li>
-            <li><a href="#">{data.about}</a></li>
-            <li><a href="/login" class="btn">{data.login_button}</a></li>
-            <li><a href="/signup" class="btn">{data.signup_button}</a></li>
-          </ul>
-        </div>
-      </div>
-    </header>
+//   if (loading) return <div>Loading...</div>;  
+//   if (error) return <div>Error: {error.message}</div>;  
+//   return (
+    
+//     <header>
+//       <div class="container">
+//         <div class="logo">
+//             <img src={LogoImg||data.logo_image_url} alt="Logo"></img>
+//             <h3>{data.logo_name}</h3>
+//         </div>
+//         <div class="links">
+//           <ul>
+//             <li><a href="#">{data.home}</a></li>
+//             <li><a href="#">{data.about}</a></li>
+//             <li><a href="/signup">Signup</a></li>
+//             <li><a href="/login">Login</a></li>
+//             </ul>
+//         </div>
+//       </div>
+//     </header>
    
-  );  
-};  
+//   );  
+// };  
 
-export default Navbar;
+// export default Navbar;
