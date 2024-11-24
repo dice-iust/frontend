4b04029fed063fa97adb88d3be855c7399159f2e
parmisvazifeh <parmisvaziferm@gmail.com>
2024-11-24 13:52:19 -0800

    Add: Added edit password functionality

--- src/Components/EditProfile/EditProfile.jsx
+++ src/Components/EditProfile/EditProfile.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { FaEdit} from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import './EditProfile.css'; 
 import profile from "./assets/profile.jpg"
@@ -8,7 +9,7 @@ import { Link } from 'react-router-dom';
 
 const EditProfile = () => {
   const [formData, setFormData] = useState({
-    profilePicture: '',
+    profile_image: '',
     firstName: '',
     lastName: '',
     user_name: '',
@@ -21,33 +22,42 @@ const EditProfile = () => {
     phone: '',
   });
 
-const [isValidPassword, setIsValidPassword] = useState(false)
-const [isValidEmail, setValidEmail] = useState(false)
+  const [isValidPassword, setIsValidPassword] = useState(false)
+  const [isValidEmail, setValidEmail] = useState(false)
+  const [image, setImage] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState)=> !prevState);
+  
+  };
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setImage(file);
+    }
+  };
 
   const getFormData = ()=>{
-    // const auth = `Bearer ${localStorage.getItem("token")}`
     axios({
-          method: 'get',
-          url: 'https://triptide.pythonanywhere.com/editprofile/update',
-          responseType: 'json',
-          // headers:{Authorization: `Bearer ${localStorage.getItem("token")}`}
-          // headers:{Authorization: auth}
+          method: "get",
+          url: "https://triptide.pythonanywhere.com/editprofile/update_2/",
+          responseType: "json",
           headers:{Authorization: localStorage.getItem("token")}
-          // headers:{Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxOCwiZXhwIjoxNzMyMzA4NzIxLCJpYXQiOjE3MzIyMjIzMjF9.E3NO9aoryxrtRIUd14e_22GXEe5na5FES_6qGxwDAu8"}
         })
-          .then(function (response) {    
+          .then(function (response) {  
+            console.log(response)
             setFormData({
-              profilePicture: response.profilePicture,
-              firstName: response.firstName,
-              lastName: response.lastName,
-              user_name: response.user_name,
-              password: response.password,
-              email: response.email,
-              bio: response.bio,
-              gender: response.gender,
-              birthDate: response.birthDate,
-              city: response.city,
-              phone: response.phone,
+              firstName: response.data.firstName,
+              lastName: response.data.lastName,
+              city: response.data.city,
+              user_name: response.data.user_name,
+              profile_image: response.data.profile_image,
+              gender: response.data.gender,
+              email: response.data.email,
+              bio: response.data.bio,
+              phone: response.data.phone,
+              birthDate: response.data.birthDate,
+              password: response.data.password,
             })
           })
           .catch(error => {
@@ -56,20 +66,23 @@ const [isValidEmail, setValidEmail] = useState(false)
         }
 
   const putDataForm = ()=>{
+    const formDataImage = new FormData();
+    formDataImage.append('image', image);
     const fdata = {
-      profilePicture: formData.profilePicture,
               firstName: formData.firstName,
               lastName: formData.lastName,
+              city: formData.city,
               user_name: formData.user_name,
-              password: formData.password,
+              profile_image: formDataImage,
+              gender: formData.gender,
               email: formData.email,
               bio: formData.bio,
-              gender: formData.gender,
-              birthDate: formData.birthDate.replace(/-/g, "/"),
-              city: formData.city,
               phone: formData.phone,
+              birthDate: formData.birthDate,
+              password: formData.password,
     }
-    axios.put('https://triptide.pythonanywhere.com/editprofile/update', fdata)
+    axios.put("https://triptide.pythonanywhere.com/editprofile/update_2/", fdata ,
+      {headers: {Authorization: localStorage.getItem("token"),'Content-Type': 'multipart/form-data',}} )
     .then(response => {
       console.log(response)
     })
@@ -80,9 +93,10 @@ const [isValidEmail, setValidEmail] = useState(false)
 
   const navigate = useNavigate();
 
-useEffect(()=>{
   const token = localStorage.getItem("token")
-  console.log(token)
+
+useEffect(()=>{
+  
   if (!token){
     navigate("/login"); 
   }
@@ -93,15 +107,15 @@ useEffect(()=>{
 const validatePassword = (password) => {  
   const hasLetters = /[a-zA-Z]/.test(password);  
   const hasNumbers = /\d/.test(password);  
-  const isLongEnough = password.length >= 8;   
+  const isLongEnough = password?.length >= 6;   
   if (!hasLetters && !hasNumbers) {  
     return 'Password must contain letters and numbers';  
   }  
   if (!hasLetters && !isLongEnough ) {  
-    return 'must also contain letters and at least 8 symbols';  
+    return 'must also contain letters and at least 6 symbols';  
   }  
   if (!hasNumbers && !isLongEnough) {  
-    return 'must also contain digits and at least 8 symbols';  
+    return 'must also contain digits and at least 6 symbols';  
   }  
   if (!hasLetters ) {  
     return 'Password must contain letters';  
@@ -110,7 +124,7 @@ const validatePassword = (password) => {
     return 'Password must contain digits';  
   }  
   if (!isLongEnough) {  
-    return 'Password must be at least 8 characters long';  
+    return 'Password must be at least 6 characters long';  
   }  
   return '';  
 };  
@@ -129,8 +143,6 @@ const validateEmail = (email) => {
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
-    console.log(e.target.value)
-
     validatePassword(formData.password) === ''? setIsValidPassword(false) : setIsValidPassword(true)
     validateEmail(formData.email) === ''? setValidEmail(false) : setValidEmail(true)
   };
@@ -143,73 +155,88 @@ const validateEmail = (email) => {
   return (
     <div className='body'>
       <div className="edit-profile-container">
-        <nav className="navbar">
-        <Link to="/Main"> <button className="nav-button" /*onClick={() => window.history.back()}*/>Back</button></Link>
-        </nav>
     
         <form className="profile-form" onSubmit={handleSubmit}>
-        <h1 className="title">Edit Profile</h1>
-        <hr />
+          <h1 className="title">Edit Profile</h1>
+          <hr />
           <div className='profile'>
-            <img src={formData.profilePicture ? formData.profilePicture : profile} alt="profile" />
+            <img src={formData.profile_image ? `http://localhost:3000/editprofile/${formData.profile_image}` : profile} alt="profile" />
           <label className="form-label ">
-            <input type="file" accept="image/*" className="file-input" onChange={(e) => {
-              setFormData({ ...formData, profilePicture: e.target.files[0].name })
-            }} />
-            <span className="change-button">Change</span>
+            <input type="file" accept="image/*" className="file-input" onChange={handleFileChange} />
+            <span className="change-button">+</span>
           </label>
           </div>
-          <div className="nameSection">
-          <label className="form-label">
-            First Name
-            <input type="text" name="firstName" className="form-input" value={formData.firstName} onChange={handleChange} />
-          </label>
-          <label className="form-label">
-            Last Name
-            <input type="text" name="lastName" className="form-input" value={formData.lastName} onChange={handleChange} />
-          </label>
-          </div>
-          <label className="form-label">
-            Username
-            <input type="text" name="user_name" className="form-input" value={formData.user_name} onChange={handleChange} />
-          </label>
-          <label className="form-label">
-            Password
-            <input type="password" name="password" className="form-input" value={formData.password} onChange={handleChange} />
-            {isValidPassword ? <div style={{color: "red",margin: "10px", fontSize:"11px"}}>invalid password</div> : null}
-          </label>
-          <label className="form-label">
-            Email
-            <input type="email" name="email" className="form-input" value={formData.email} onChange={handleChange} />
-            {isValidEmail ? <div style={{color: "red",margin: "10px", fontSize:"11px"}}>invalid email</div> : null}
-          </label>
-          <label className="form-label">
-            Birth Date
-            <input type="date" name="birthDate" className="form-input" value={formData.birthDate} onChange={handleChange} />
-          </label>
-          <label className="form-label">
-            Gender
-            <select name="gender" className="form-input" onChange={handleChange}>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="other">Others</option>
-            </select>
-            </label>
-          <label className="form-label">
-            City
-            <input type="text" name="city" className="form-input" value={formData.city} onChange={handleChange} />
-          </label>
-          <label className="form-label">
-            Phone
-            <input type="tel" name="phone" className="form-input" value={formData.phone} onChange={handleChange} />
-          </label>
-            <label className="form-label">
-              Bio
-              <textarea name="bio" className="form-textarea" value={formData.bio} onChange={handleChange} />
-            </label>
-            <div className='submit'>
-                <button type="submit" className="submit-button">Submit</button>
+
+          <div className="input-container">
+            <div className="input-div">
+              <label  className="form-label">First Name</label>
+              <input type="text" name="firstName" className="form-input" value={formData.firstName} onChange={handleChange} />
             </div>
+            <div  className="input-div">
+              <label className="form-label">Last Name</label>
+              <input type="text" name="lastName" className="form-input" value={formData.lastName} onChange={handleChange} />
+            </div>
+          </div>
+          
+          <div className="input-container">
+            <div className="input-div">
+              <label className="form-label">Username</label>
+              <input type="text" name="user_name" className="form-input" value={formData.user_name} onChange={handleChange} />
+            </div>
+
+            <div  className="input-div">
+              <label  className="form-label">Password</label>
+              <div className='edit-pwd' onClick={togglePasswordVisibility}><FaEdit/></div>
+              <input type={showPassword ? 'text' : 'password' } name="password" className="form-input" value={formData.password} onChange={handleChange} />
+                {isValidPassword ? <div style={{color: "red",marginTop: "3px",marginLeft:"6px", fontSize:"11px"}}>invalid password</div> : null}
+            </div>
+          </div>
+
+          <div className="input-container">
+            <div className="input-div">
+              <label  className="form-label">City</label>
+              <input type="text" name="city" className="form-input" value={formData.city} onChange={handleChange} />
+            </div>
+
+            <div  className="input-div">    
+              <label className="form-label">Phone</label>
+              <input type="tel" name="phone" className="form-input" value={formData.phone} onChange={handleChange} />
+            </div>
+          </div>
+
+          <div className="input-container">
+            <div className="input-div">
+              <label className="form-label">Email</label>
+              <input type="email" name="email" className="form-input" value={formData.email} onChange={handleChange} />
+              {isValidEmail ? <div style={{color: "red",marginTop: "3x",marginLeft:"6px", fontSize:"11px"}}>invalid email</div> : null}
+            </div>
+
+            <div  className="input-div">
+              <label className="form-label">Birth Date</label>
+              <input type="date" name="birthDate" className="form-input" value={formData.birthDate} onChange={handleChange} />   
+            </div>
+          </div>
+
+          <div className="input-container">
+            <div className="input-div">
+              <label className="form-label">Gender</label>
+              <select name="gender" className="form-select" onChange={handleChange}>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Other">Others</option>
+              </select>
+            </div>
+
+          </div>
+            <div style={{width:"100%"}}>
+              <label className="form-label">Bio</label>
+              <textarea name="bio" className="form-textarea" rows={8} value={formData.bio} onChange={handleChange} />
+            </div>
+        
+          <div className='submit'>
+              <button type="submit" className="submit-button">Submit</button>
+          </div>
+ 
         </form>
       </div>
     </div>
