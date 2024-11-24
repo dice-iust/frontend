5290342b7a44cecc929688ea1b7e01cf44f2a472
Masumesk <msumsanatkar@gmail.com>
2024-11-25 00:42:51 +0330

    fix : fix header margin

--- src/Components/tourspage/categories_nav.jsx
+++ src/Components/tourspage/categories_nav.jsx
@@ -7,25 +7,26 @@ import { Link } from 'react-router-dom';
 function Navbar_category() {
 	const navref = useRef();
   const showNavbar = () => {
-    navref.current.classList.toggle("responsive_nav");
+    navref.current.classList.toggle("responsive_nav-category");
   };
 
 	return (
-		<div className="nav-page">
+		<div className="nav-page-category">
       <div class="home-back-category">
         <header>
-          <div class="logo">
-              <img src={LogoImg} alt="Logo"></img>
-              <h3 className="trip-title" >Trip Tide</h3> 
+          <div class="logo-category">
+              <img src={LogoImg} alt="Logo-category"></img>
+              <h3 className="trip-title-category" >Trip Tide</h3> 
           </div>
             <nav ref={navref}>  
-              <button className="nav-btn nav-close-btn" onClick={showNavbar}>  
+              <button className="nav-btn-category nav-close-btn-category" onClick={showNavbar}>  
                 <FaTimes />  
               </button>  
+              <Link to="/Main"><a href="/">Home</a></Link>
               <Link to="/about"><a href="/">About</a></Link>
               <Link to="/EditProfile"><a href="/">Profile</a> </Link> 
             </nav>  
-            <button className="nav-btn" onClick={showNavbar}>  
+            <button className="nav-btn-category" onClick={showNavbar}>  
               <FaBars />  
             </button>
         </header>
