674150944107790298729dad86e4e087853cb811
Masumesk <msumsanatkar@gmail.com>
2024-11-29 23:52:27 +0330

    fix : fix scss

--- src/Components/ProfilePage/ProfilePage.jsx
+++ src/Components/ProfilePage/ProfilePage.jsx
@@ -1,26 +1,31 @@
 import React from 'react';  
-import "./ProfilePage.scss"
+import "./ProfilePage.scss";  
 
 const Profile = () => {  
   return (  
-    <div>  
-      <nav className="menu" tabIndex="0">  
-        <div className="smartphone-menu-trigger"></div>  
+    <div className="profile-page">  
+      <nav className="sidebar-menu" tabIndex="0">  
+        <div className="smartphone-menu-trigger" aria-label="Menu Trigger"></div>  
         <header className="avatar">  
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqafzhnwwYzuOTjTlaYMeQ7hxQLy_Wq8dnQg&s" alt="John D." />  
+          <img   
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqafzhnwwYzuOTjTlaYMeQ7hxQLy_Wq8dnQg&s"   
+            alt="Profile of John D."   
+            className="avatar-image"   
+          />  
           <h2>John D.</h2>  
         </header>  
-        <ul>  
-          <li tabIndex="0" className="icon-dashboard"><span>Dashboard</span></li>  
-          <li tabIndex="0" className="icon-customers"><span>Customers</span></li>  
-          <li tabIndex="0" className="icon-users"><span>Users</span></li>  
-          <li tabIndex="0" className="icon-settings"><span>Settings</span></li>  
+        <ul aria-label="Main Navigation Menu">  
+          <li tabIndex="0" className="icon-dashboard" role="menuitem"><span>My Trips</span></li>  
+          <li tabIndex="0" className="icon-settings" role="menuitem"><span>My Rate</span></li>  
+          <li tabIndex="0" className="icon-customers" role="menuitem"><span>Edit Profile</span></li>  
+          <li tabIndex="0" className="icon-users" role="menuitem"><span>Create Trip</span></li>  
         </ul>  
       </nav>  
       <main>  
         <div className="helper">  
-          RESIZE THE WINDOW  
-          <span>Breakpoints on 900px and 400px</span>  
+          <h1>Profile Overview</h1>  
+          <p>RESIZE THE WINDOW</p>  
+          <span>Breakpoints at 900px and 400px</span>  
         </div>  
       </main>  
     </div>  
