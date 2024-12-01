3dcf5e20550d32c60a14c7c6de00719a0a19d4e9
dorsamrh <dorsa.mrh82@gmail.com>
2024-12-01 17:47:02 +0330

    Feat : add header and footer to profile page

--- src/Components/ProfilePage/ProfilePage.jsx
+++ src/Components/ProfilePage/ProfilePage.jsx
@@ -1,33 +1,39 @@
 import React from 'react';  
 import "./ProfilePage.scss";  
+import Travelsnav from "../tourspage/categories_nav.jsx";  
+import Footer from '../tourspage/footer.jsx';  
 
 const Profile = () => {  
   return (  
-    <div className='profile'>  
-      <nav className="menu" tabIndex="0">  
-        <div className="smartphone-menu-trigger"></div>  
-        <header className="avatar">  
-          <img   
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqafzhnwwYzuOTjTlaYMeQ7hxQLy_Wq8dnQg&s"   
-            alt="Profile of John D."   
-            className="avatar-image"   
-          />  
-          <h2>John D.</h2>  
-        </header>  
-        <ul>  
-          <li tabIndex="0" className="icon-dashboard"><span>My trips</span></li>  
-          <li tabIndex="0" className="icon-customers"><span>My rate</span></li>  
-          <li tabIndex="0" className="icon-users"><span>Create new trip</span></li>  
-          <li tabIndex="0" className="icon-settings"><span>Edit profile</span></li>  
-        </ul>  
-      </nav>  
-      <main>  
-        <div className="helper">  
-          <h1>Profile Overview</h1>  
-          <p>RESIZE THE WINDOW</p>  
-          <span>Breakpoints at 900px and 400px</span>  
-        </div>  
-      </main>  
+    <div className='ProfilePage'>  
+      <Travelsnav/>      
+      <div className='profile'>  
+        <nav className="menu" tabIndex="0">  
+          <div className="smartphone-menu-trigger"></div>  
+          <header className="avatar">  
+            <img   
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqafzhnwwYzuOTjTlaYMeQ7hxQLy_Wq8dnQg&s"   
+              alt="Profile of John D."   
+              className="avatar-image"   
+            />  
+            <h2>John D.</h2>  
+          </header>  
+          <ul>  
+            <li tabIndex="0" className="icon-dashboard"><span>My trips</span></li>  
+            <li tabIndex="0" className="icon-customers"><span>My rate</span></li>  
+            <li tabIndex="0" className="icon-users"><span>Create new trip</span></li>  
+            <li tabIndex="0" className="icon-settings"><span>Edit profile</span></li>  
+          </ul>  
+        </nav>  
+        <main className="content">  
+          <div className="helper">  
+            <h1>Profile Overview</h1>  
+            <p>RESIZE THE WINDOW</p>  
+            <span>Breakpoints at 900px and 400px</span>  
+          </div>  
+        </main>  
+      </div>  
+      <Footer/>  
     </div>  
   );  
 };  
