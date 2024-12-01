4799470dc7f65f88e55df5001679ef4a6fa1680a
dorsamrh <dorsa.mrh82@gmail.com>
2024-12-01 22:32:33 +0330

    Add : add my trips to profile page

--- src/Components/ProfilePage/MyTrips/MyTrips.jsx
+++ src/Components/ProfilePage/MyTrips/MyTrips.jsx
@@ -1,9 +1,8 @@
 // import React from 'react'; 
 import './MyTrips.scss'; 
-// import React, { useState } from 'react';  
-import axios from "../../../api/axios.js";  
+
+import axios  from "../../../api/axios.js";
 import React, { useEffect, useState } from "react"; 
-// import { MdDateRange } from "react-icons/md";
 import { GrMoney } from "react-icons/gr";
 import { FaCarSide } from "react-icons/fa6";
 import { FaPlane } from "react-icons/fa";
@@ -14,118 +13,171 @@ import { FaRegCalendar } from "react-icons/fa6";
 import "slick-carousel/slick/slick.css";   
 import "slick-carousel/slick/slick-theme.css";  
 import Slider from "react-slick"; 
-// import Travelsnav from "../Nav.jsx";
-// import populartours from "./populartours.jsx";
+import { FaArrowRight } from "react-icons/fa";
+import { IoIosArrowBack } from "react-icons/io";
+import { Link } from 'react-router-dom'; 
 
 
 
+const PopularTravels_URL = 'travels/';
 
- 
 
+const TourList = () => { 
 
-const MyTrips = () => { 
-    const tours = [  
-        {  
-          Id: 1,  
-          name: "City Trip1",  
-          start_date: "2023-03-15",  
-          image_url: "https://plus.unsplash.com/premium_photo-1697729905164-f61ad5207758?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVocmFufGVufDB8fDB8fHww",
-          destination: "Tehran",  
-          admin: { user_name: "Admin5", phrofile_image: "https://clipart-library.com/2023/Admin-Profile-Vector-PNG-Clipart.png" }, 
-          mode : "Budget-Friendly" , 
-          start_place : "Isfahan",
-          transportation : "Car",
-         end_date : "2023-11-27",
-        }
-    ]
-
-    const formatDate = (dateString) => {  
-        const [year, month, day] = dateString.split('-');  
-        return `${year}/${month}/${day}`;  
+  const [data, setData] = useState(null);
+  useEffect(() => {  
+      const fetchData = async () => {  
+          try {  
+              const response = await axios.get(PopularTravels_URL);  
+              setData(response.data);  
+              console.log(response.data);
+          } catch (error) {  
+              console.error("Error fetching data:", error);  
+          }  
       };  
+      fetchData();  
+  }, []);  
+    
+  
+  const formatDate = (dateString) => {  
+    const [year, month, day] = dateString.split('-');  
+    return `${year}/${month}/${day}`;  
+  };  
 
   const getTransportationIcon = (transportation) => {  
     switch (transportation.toLowerCase()) {  
       case 'train':  
-        return <TbTrain className="moveicon-mytrips" />;  
+        return <TbTrain className="moveicon"/>;  
       case 'bus':  
-        return <TbBus className="moveicon-mytrips"/>;  
+        return <TbBus className="moveicon" />;  
       case 'plane':  
-        return <FaPlane className="moveicon-mytrips" />;  
+        return <FaPlane className="moveicon" />;  
       case 'car':  
-        return <FaCarSide className="moveicon-mytrips" />;  
+        return <FaCarSide className="moveicon"/>;  
       default:  
         return null;  
     }  
   };
-  
-    const currentDate = new Date();  
-    
-    return (  
-    <div className='app-container-mytrips'>
-      <div className='mytrips'>  
-      
-     
-      <br/>
-      <div className="tour-list-container-mytrips">  
-           
-              <div className="tour-list-mytrips">  
-                  {tours.map((tour) => (  
-                      <div key={tour.Id} className="tour-card-mytrips">  
-                          <div className="tour-image-container-mytrips">  
-                              <img  
-                                  src={tour.image_url}  
-                                  alt={`Image of ${tour.name}`}  
-                                  className="tour-image-mytrips"  
-                              />  
-                              {tour.admin && (  
-                                  <div className="tour-admin-mytrips">  
-                                      <img  
-                                          src={tour.admin.phrofile_image}   
-                                          alt={`Profile of ${tour.admin.user_name}`}  
-                                          className="admin-photo-mytrips"  
-                                      />  
-                                      {tour.admin.user_name}   
-                                  </div>  
-                              )}  
-                          </div>  
-                          <div className="tour-info-mytrips">  
-                              <p className="tour-meta-mytrips3">  
-                                  <span className="tour-name-mytrips">{tour.name}</span>  
-                                  <div className={`trip-type-mytrips ${tour.mode}`}>  
-                                      <GrMoney aria-hidden="true" />{" "}  
-                                      {tour.mode.charAt(0).toUpperCase() + tour.mode.slice(1)}  
-                                  </div>  
-                              </p>  
-                              <div className="tour-details-mytrips">  
-                                  <p className="tour-route-mytrips">  
-                                      <span className="tour-text-mytrips">{tour.start_place} {getTransportationIcon(tour.transportation)} {tour.destination}</span>  
-                                  </p>  
-                              </div>  
-                              <div className="tour-meta-mytrips7">  
-                                  <p className="tour-dates-mytrips">  
-                                      <FaRegCalendar className='moveicon-mytrips3' />  
-                                      <span>{formatDate(tour.start_date)}</span>  
-                                  </p>  
-                                  <p className="tour-length-mytrips" style={{ textAlign: "left" }}>  
-                                      <FaUndoAlt className='moveicon-mytrips3' />  
-                                      {formatDate(tour.end_date)}  
-                                  </p>  
-                              </div>   
-                          </div>  
-                      </div>  
-                  ))}  
-              </div>  
-          
-      </div>  
-      </div> 
-      
-      <br/>
-     
-  </div>  
-  );  
-};  
-export default MyTrips;
-    
 
+    const currentDate = new Date();  
+    const currentYear = currentDate.getFullYear();  
+    const currentMonth = currentDate.getMonth();   
+
+  const settings = {  
+    dots: true,  
+    infinite: true,  
+    speed: 500,  
+    slidesToShow: 3,  
+    slidesToScroll: 1, 
+    centerMode: true, 
+               
+    centerPadding: '236px', 
+    
+    
+    
+   
+    
+    responsive: [ 
+      {  
+        breakpoint: 1200,  
+        settings: {  
+          slidesToShow: 2,  
+          slidesToScroll: 1,
+          // centerPadding: '0px'  
+        }  
+      },  
+      {  
+        breakpoint: 1024,  
+        settings: {  
+          slidesToShow: 2,  
+          slidesToScroll: 1,
+          centerPadding: '0px'  
+        }  
+      },  
+      {  
+        breakpoint: 600,  
+        settings: {  
+          slidesToShow: 1,  
+          slidesToScroll: 1,  
+        }  
+      }  
+    ]  
+  };  
+
+  return ( 
+    <div className='app-container-main'> 
+    <div className='travelpage'>
+    <div className="tour-list-container"> 
+    
+<br></br>
+<br></br>
+      <h1>Popular Trips</h1> 
+      <br></br> 
+      {data && data.Popular_Trips ? (  
+          <Slider {...settings}>  
+            {data.Popular_Trips.slice(0, 5).map((tour) => (  
+              <div key={tour.id} className="tour-card">  
+                <div className="tour-image-container">  
+                  <img  
+                    src={tour.image_url}  
+                    alt={`Image of ${tour.name}`}  
+                    className="tour-image"  
+                  />  
+                  {tour.admin && (  
+                    <div className="tour-admin">  
+                      <img  
+                        src={tour.admin.phrofile_image}  
+                        alt={`Profile of ${tour.admin.user_name}`}  
+                        className="admin-photo"  
+                      />  
+                      {tour.admin.user_name}  
+                    </div>  
+                  )}  
+                </div>  
+                <div className="tour-info">  
+                  <p className="tour-meta3">  
+                    <span className="tour-name">{tour.name}</span>  
+                    <div className={`trip-type ${tour.mode}`}>  
+                      <GrMoney aria-hidden="true" />  
+                      {tour.mode.charAt(0).toUpperCase() + tour.mode.slice(1)}  
+                    </div>  
+                  </p>  
+                  <div className="tour-details">  
+                    <p className="tour-route">  
+                      <span className="tour-text">{tour.start_place} {getTransportationIcon(tour.transportation)} {tour.destination}</span>  
+                    </p>  
+                  </div>  
+                  <div className="tour-meta">  
+                    <p className="tour-date">  
+                      <FaRegCalendar className='moveicon3' />  
+                      <span>{formatDate(tour.start_date)}</span>  
+                    </p>  
+                    <FaArrowRight className='moveicon4' />  
+                    <p className="tour-length" style={{ textAlign: "center" }}>  
+                      <FaUndoAlt className='moveicon3' />  
+                      {formatDate(tour.end_date)}  
+                    </p>  
+                  </div>  
+                </div>  
+              </div>  
+            ))}  
+          </Slider>  
+        ) : (  
+          <p>Loading popular trips...</p>  
+        )} 
+        </div> 
+<br></br>
+<br></br>
+<br></br>
+      
+      
+       
+      </div>  
+       </div>
+    
+);  
+};  
+
+
+export default TourList;  
 
