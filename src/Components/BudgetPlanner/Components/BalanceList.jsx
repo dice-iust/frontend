235be286fa9b09d1cef891779f0be327793e83bb
dorsamrh <dorsa.mrh82@gmail.com>
2024-12-15 13:56:38 +0330

    fix :  fix balance list appearence

--- src/Components/BudgetPlanner/Components/BalanceList.jsx
+++ src/Components/BudgetPlanner/Components/BalanceList.jsx
@@ -18,25 +18,25 @@ const BalanceList = ({ balances, setBalances }) => {
 
         return (  
 
-            <div className="w3-container balance-container">  
+            <div className="w3-container-b balance-container">  
 
                 <br/>
-                <div className="sidebar"> {/* Sidebar for tabs */}  
-                    <div className={`tablink ${activeTab === 'Debts' ? 'w3-border-red' : ''}`} onClick={() => openCity('Debts')}>  
+                <div className="sidebar-b"> {/* Sidebar for tabs */}  
+                    <div className={`tablink-b ${activeTab === 'Debts' ? 'w3-border-red' : ''}`} onClick={() => openCity('Debts')}>  
                         Debts  
                     </div>  
-                    <div className={`tablink ${activeTab === 'Receivables' ? 'w3-border-red' : ''}`} onClick={() => openCity('Receivables')}>  
+                    <div className={`tablink-b ${activeTab === 'Receivables' ? 'w3-border-red' : ''}`} onClick={() => openCity('Receivables')}>  
                         Receivables  
                     </div>  
-                    <div className={`tablink ${activeTab === 'Payments' ? 'w3-border-red' : ''}`} onClick={() => openCity('Payments')}>  
+                    <div className={`tablink-b ${activeTab === 'Payments' ? 'w3-border-red' : ''}`} onClick={() => openCity('Payments')}>  
                         Past Payments   
                     </div>  
                 </div>  
   
-                <div className="content"> {/* Content for selected tab */}  
+                <div className="content-b"> {/* Content for selected tab */}  
                     <h2 style={{ color: "#22487a" }}>My {activeTab}</h2>  
                     <br/>     
-                    <div className="balance-list">  
+                    <div className="balance-list-b">  
                         {balances.length >= 1 ? (  
                           balances.map((item) => (  
                             <div key={item.id} className="balance-item" style={{ color: item.type === 'owing' ? 'red' : 'green' }}>  
