c3a76ef082e812882cc605e08ce29d928bb88f60
Masumesk <msumsanatkar@gmail.com>
2024-12-17 00:17:38 +0330

    fix : fix some margins

--- src/Components/ChatBox/Components/MessageBox.jsx
+++ src/Components/ChatBox/Components/MessageBox.jsx
@@ -1,22 +1,26 @@
-import React from "react";
-import { Typography } from "@mui/material";
-import "./MessageBox.scss";
+import React from "react";  
+import { Typography } from "@mui/material";  
+import "./MessageBox.scss";  
 
-const MessageBox = (props) => {
-  const { messageItems } = props;
+const MessageBox = (props) => {  
+  const { messageItems } = props;  
 
-  return (
-    <div className="message-container" key={messageItems.id}>
-      <img src={messageItems.profilePic} alt="profile" />
-      <div className="textMessage">
-        {messageItems.isAdmin ? (
-          <Typography sx={{ color: "gray" }}>Admin</Typography>
-        ) : null}
-        <Typography>{messageItems.textMessage}</Typography>
-        <Typography sx={{ color: "gray" }}>{messageItems.date}</Typography>
-      </div>
-    </div>
-  );
-};
+  return (  
+    <div className={`message-container ${messageItems.isAdmin ? "admin-message" : "user-message"}`} key={messageItems.id}>  
+      <img src={messageItems.profilePic} alt="profile" />  
+      <div className="textMessage">  
+        {messageItems.isAdmin && (  
+          <Typography variant="subtitle2" sx={{ color: "red", fontWeight: "bold" }}>  
+            Admin  
+          </Typography>  
+        )}  
+        <Typography variant="body1">{messageItems.textMessage}</Typography>  
+        <Typography variant="caption" sx={{ color: "gray" }}>  
+          {messageItems.date}  
+        </Typography>  
+      </div>  
+    </div>  
+  );  
+};  
 
 export default MessageBox;
