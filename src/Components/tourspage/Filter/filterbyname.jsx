884890ad2419c44d911fced7a5b53b454eec733f
Masumesk <msumsanatkar@gmail.com>
2024-12-20 00:41:34 +0330

    Add : connecting other filter options to back

--- src/Components/tourspage/Filter/filterbyname.jsx
+++ src/Components/tourspage/Filter/filterbyname.jsx
@@ -116,39 +116,38 @@ const NamePicker = () => {
       setNameError('');   
     }   
     if (!hasError) {    
-    //   await fetchTravelData(Namedata);  
+      await fetchTravelData(Namedata);  
     } 
     resetNamePickers();  
   };  
 
-//   const fetchTravelData = async (tname) => {  
-//     try {  
-//       const response = await axios.get(`https://triptide.pythonanywhere.com//travels/filter/`, {  
-//         params: {  
-//           start_date: start,  
-//           end_date: end,  
-//         },  
-//       });  
-//       console.log('Response from backend:', response.data);  
-//       setResultData(response.data);  
+  const fetchTravelData = async (namedata) => {  
+    try {  
+      const response = await axios.get(`https://triptide.pythonanywhere.com//travels/filter/`, {  
+        params: {  
+          name : namedata
+        },  
+      });  
+      console.log('Response from backend:', response.data);  
+      setResultData(response.data);  
 
       
-//       if (response.data.length === 0) {  
-//         setIstrip(false);
-//         // setNoTripsMessage('No trips available for the selected dates.Create your own unique journey!'); 
-//       } else {  
-//         setIstrip(true);
-//         setisname(tname)
-//         // setNoTripsMessage(`your travel options for the dates ${start} to ${end}`); // Clear the message if trips are available  
-//       }  
-//     } catch (error) {  
-//       console.error('Error fetching data from backend:', error);  
-//       setResultData([]);   
-//       // setNoTripsMessage('Error fetching data.'); // Optional error message  
-//     }  
+      if (response.data.length === 0) {  
+        setIstrip(false);
+        
+      } else {  
+        setIstrip(true);
+        setName(namedata);
+        
+      }  
+    } catch (error) {  
+      console.error('Error fetching data from backend:', error);  
+      setResultData([]);   
+      
+    }  
 
     
-//   };  
+  };  
 
   const resetNamePickers = () => {  
     setName(null);  
