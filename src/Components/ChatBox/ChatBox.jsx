db802f702aa7da30caa3491e0fbcbc5ed1a38bfc
dorsamrh <dorsa.mrh82@gmail.com>
2024-12-21 11:43:16 +0330

    fix : connect realtime chat to back

--- src/Components/ChatBox/ChatBox.jsx
+++ src/Components/ChatBox/ChatBox.jsx
@@ -4,25 +4,63 @@ import "./ChatBox.scss";
 import MessageBox from "./Components/MessageBox";  
 import { messageData } from "../../../src/api/jsondata/planner"; 
 import Ably from 'ably' ;
+import axios from "../../api/axios";
 
 const ChatBox = ({tourname}) => {  
   const [message, setMessage] = useState("");  
   const ably =new Ably.Realtime('w9hDjQ.bDJwDg:nV7gxEThhWT4clJqHv9K3syB3SQCDrkcgaoChiWmRQY');
   const channel =ably.channels.get(`travel_${tourname}`);
+  const [error, setError] = useState(null);  
+  const [loading, setLoading] = useState(true);  
   channel.subscribe('chat',(message)=>{
     console.log('Received: ',message.data);
   });
+    // useEffect(() => {  
+        const sendMessage = async (message,tourname,token) => {  
+            if (!tourname) {  
+                setError('Tour name is required.');  
+                setLoading(false);  
+                return;  
+            }  
+            try {  
+                const response = await axios.post(`https://triptide.pythonanywhere.com/publish_message/`,{
+                  message,travel_name: tourname
+                }, {  
+                    headers: {  
+                        Authorization: token,  
+                    },   
+                });  
+                console.log("data from back: ",response.data);
+                // if (response.data.travels.travel_is.start_date) 
+                // { 
+                //     setIsAdmin(true);  
+                //     setcode(response.data.code); 
+                // }
+                
+                // setTripData(response.data);  
+            }   
+            catch (err) {    
+              setError(err.response?.data?.detail || 'An error occurred while fetching trip data.');  
+            }   
+            finally {  
+              setLoading(false);  
+            }  
+        };  
+    
+        // fetchTripData();   
+    // }, [tourname]);
+
   const handleChange = (e) => {  
-    const { value } = e.target;  
-    setMessage(value);  
+    setMessage(e.target.value);  
   };  
 
-  const handleSendMessage = () => {  
-    if (message.trim()) {  
-      // Send the message (to be implemented)  
+  const handleSendMessage = async (e) => {  
+    e.preventDefault();
+    if (message.trim()==='')  return;
       console.log("Sending message:", message);  
+      await sendMessage(message,tourname,localStorage.getItem("token"));
       setMessage(""); // Clear the input after sending  
-    }  
+     
   };  
 
   return (  
